b1ec9163ea686826a0b4b674ecc0418f2fbe238d
yoon123seul <114347488+yoon123seul@users.noreply.github.com>
2023-11-25 01:18:32 +0900

    Update model.py

--- Optimizers/model.py
+++ Optimizers/model.py
@@ -14,3 +14,28 @@ class Model(nn.Module):
         x = self.linear4(x)
 
         return x
+optimzers = ["SGD", "Adam", "AdamW"]
+result = {}
+for opt in optimzers:
+    train_loss_opt = []
+    test_accuracy_opt = []
+    if opt == "SGD":
+            optimizer = torch.optim.SGD(TestModel.parameters(), lr=learning_rate)
+    elif opt == "Adam":
+            optimizer = torch.optim.Adam(TestModel.parameters(), lr=learning_rate)
+    elif opt == "AdamW":
+            optimizer = torch.optim.AdamW(TestModel.parameters(), lr=learning_rate)
+    
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1) ##########
+
+    for epoch in range(nb_epochs):
+        train_loss = train(TestModel, trainloader, optimizer, criterion)
+        test_accuracy = test(TestModel, testloader, criterion)
+        scheduler.step()
+        print(f'Epoch {epoch+1}, Training Loss: {train_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
+        train_loss_opt.append(train_loss)
+        test_accuracy_opt.append(test_accuracy)
+
+    result[opt] = {"Train_loss" : train_loss_opt, "Accuracy" : test_accuracy_opt}
+
+print (result)
