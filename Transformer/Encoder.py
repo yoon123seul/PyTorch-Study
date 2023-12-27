0d990fe69d1211d5bec2c0fafea2cbc5cb95ba6b
yoon123seul <114347488+yoon123seul@users.noreply.github.com>
2023-12-27 16:23:36 +0900

    Create Encoder.py

---
+++ Transformer/Encoder.py
@@ -0,0 +1,42 @@
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout_ratio, device, max_length=100):
+        super().__init__()
+
+        self.device = device
+
+        self.tok_embedding = nn.Embedding(input_dim, hidden_dim)
+        self.pos_embedding = nn.Embedding(max_length, hidden_dim)
+
+        self.layers = nn.ModuleList([EncoderLayer(hidden_dim, n_heads, pf_dim, dropout_ratio, device) for _ in range(n_layers)])
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+        self.scale = torch.sqrt(torch.FloatTensor([hidden_dim])).to(device)
+
+    def forward(self, src, src_mask):
+
+        # src: [batch_size, src_len]
+        # src_mask: [batch_size, src_len]
+
+        batch_size = src.shape[0]
+        src_len = src.shape[1]
+
+        if src_len > 100:
+            print(src_len)
+
+        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+
+        # pos: [batch_size, src_len]
+
+        # 소스 문장의 임베딩과 위치 임베딩을 더한 것을 사용
+        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        # 모든 인코더 레이어를 차례대로 거치면서 순전파(forward) 수행
+        for i, layer in enumerate(self.layers):
+            src = layer(src, src_mask)
+
+        # src: [batch_size, src_len, hidden_dim]
+
+        return src # 마지막 레이어의 출력을 반환
