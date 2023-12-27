94de5b7c65715a907e8a133150ac2f80078df27b
yoon123seul <114347488+yoon123seul@users.noreply.github.com>
2023-12-27 16:22:33 +0900

    Create PositionwiseFeedforwardLayer.py

---
+++ Transformer/PositionwiseFeedforwardLayer.py
@@ -0,0 +1,22 @@
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hidden_dim, pf_dim, dropout_ratio):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(hidden_dim, pf_dim)
+        self.fc_2 = nn.Linear(pf_dim, hidden_dim)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, x):
+
+        // x: [batch_size, seq_len, hidden_dim]
+
+        x = self.dropout(torch.relu(self.fc_1(x)))
+
+        // x: [batch_size, seq_len, pf_dim]
+
+        x = self.fc_2(x)
+
+        // x: [batch_size, seq_len, hidden_dim]
+
+        return x
